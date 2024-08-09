8f21754d87486df6b76280c1357495619f2369db
kuzin_sn <kuzinnatra@yandex.ru>
2024-08-09 19:39:36 +0400

    Реализован построчный вывод результатов

--- main.py
+++ main.py
@@ -24,29 +24,37 @@ def main():
 
 
     while True:
-        user_input = input('Введите номер запроса\n')
+        user_input = input('\nВведите номер запроса:\n')
         if user_input == "1":
             companies_and_vacancies_count = db_manager.get_companies_and_vacancies_count()
-            print(f"Список всех компаний и количество вакансий у каждой компании: {companies_and_vacancies_count}\n")
+            print("Список всех компаний и количество вакансий у каждой компании:")
+            for i in companies_and_vacancies_count:
+                print(i)
 
         elif user_input == "2":
             all_vacancies = db_manager.get_all_vacancies()
-            print(
-                f"Cписок всех вакансий с указанием названия компании, названия вакансии и зарплаты и ссылки на вакансию: {all_vacancies}\n")
+            print("Cписок всех вакансий с указанием названия компании, названия вакансии, зарплаты и ссылки на вакансию:")
+            for i in all_vacancies:
+                print(i)
 
         elif user_input == '3':
             avg_salary = db_manager.get_avg_salary()
-            print(f"Средняя зарплата по вакансиям: {avg_salary}\n")
+            print("Средняя зарплата по вакансиям:")
+            for i in avg_salary:
+                print(i)
 
         elif user_input == "4":
             vacancies_with_higher_salary = db_manager.get_vacancies_with_higher_salary()
-            print(
-                f"Список всех вакансий, у которых зарплата выше средней по всем вакансиям: {vacancies_with_higher_salary}\n")
+            print("Список всех вакансий, у которых зарплата выше средней по всем вакансиям:")
+            for i in vacancies_with_higher_salary:
+                print(i)
 
         elif user_input == "5":
             user_input = input('Введите ключевое слово ')
             vacancies_with_keyword = db_manager.get_vacancies_with_keyword(user_input)
-            print(f"Список всех вакансий, в названии которых содержатся запрашиваемое слово: {vacancies_with_keyword}")
+            print("Список всех вакансий, в названии которых содержатся запрашиваемое слово:")
+            for i in vacancies_with_keyword:
+                print(i)
 
         elif user_input == '0':
             break
