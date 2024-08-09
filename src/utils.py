fef1fe73ce68417c3b55046ce0479d362ba79da3
kuzin_sn <kuzinnatra@yandex.ru>
2024-08-09 17:48:15 +0400

    Реализована функция создания БД и таблиц

--- src/utils.py
+++ src/utils.py
@@ -28,3 +28,45 @@ def get_vacancies_data():
         vacancy_info = requests.get(url_vac, params={'page': 0, 'per_page': 100}).json()
         vacancy.extend(vacancy_info['items'])
     return vacancy
+
+
+def create_database(database_name: str, params: dict) -> None:
+    """
+    функция для создания Базы Данных и создания таблиц в БД
+    """
+    conn = psycopg2.connect(dbname='postgres', **params)
+    conn.autocommit = True
+    cur = conn.cursor()
+
+    cur.execute(f'DROP DATABASE IF EXISTS {database_name}')
+    cur.execute(f'CREATE DATABASE {database_name}')
+
+    conn.close()
+
+    conn = psycopg2.connect(dbname=database_name, **params)
+
+    with conn.cursor() as cur:
+        cur.execute("""
+            CREATE TABLE employers (
+                employer_id INTEGER,
+                employer_name text not null,
+                employer_area TEXT not null,
+                url TEXT,
+                open_vacancies INTEGER
+            )
+        """)
+
+    with conn.cursor() as cur:
+        cur.execute("""
+            CREATE TABLE vacancy (
+                vacancy_id INTEGER,
+                vacancy_name VARCHAR,
+                vacancy_area VARCHAR,
+                salary INTEGER,
+                employer_id INTEGER,
+                vacancy_url VARCHAR
+            )
+        """)
+
+    conn.commit()
+    conn.close()
